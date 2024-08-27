58dca78c244cbd59e971f7fb37b6ec8e3baa83bb
Yuan (Greg) Wang <yuan@gregw.xyz>
2024-08-27 11:29:10 -0400

    re-code printing func

--- jngram.c
+++ jngram.c
@@ -4,7 +4,7 @@
  *    Description:  ngram generator for json keywords
  *        Created:  2024-08-26
  *       Compiler:  gcc
- *         Author:  Y. Wang
+ *         Author:  Y. WANG
  * ==============================================================
  */
 #include <stdio.h>
@@ -19,18 +19,12 @@ const char *usage = "Usage: jngram [flags] filename\n"
 	"\n"
 	"Flags:\n"
 	"\t-l num  minimal length of search keywords (default: 3);\n"
-	"\t-r      print raw keyword ngrams (default: formatted css code).\n";
+	"\t-r      print raw keyword ngrams (default: formatted css code);\n";
 
 #define MAX_STRING_LEN 200
 #define MAX_KEYWORD_NUM 100 // for one book
 #define MAX_KEYWORD_LEN 50
 	// TODO: error handling on overstepping these limits
-
-long get_mem_usage(){
-	struct rusage myusage;
-	getrusage(RUSAGE_SELF, &myusage);
-	return myusage.ru_maxrss;
-}
 
 struct kwdata {
 	char vkeyword[MAX_KEYWORD_NUM][MAX_KEYWORD_LEN];
@@ -46,12 +40,12 @@ int keyword_exists (struct kwdata *keywords, char *new_keyword) {
 	return 0;
 }
 
-void add_keyword(struct kwdata *keywords, char *new_keyword) {
+void keyword_add(struct kwdata *keywords, char *new_keyword) {
 	strcpy(keywords->vkeyword[keywords->nkeyword], new_keyword);
 	keywords->nkeyword++;
 }
 
-void keyword_init(struct kwdata *keywords) {
+void keywords_init(struct kwdata *keywords) {
 	memset(keywords->vkeyword, 0, sizeof(keywords->vkeyword));
 	keywords->nkeyword = 0;
 }
@@ -71,26 +65,9 @@ void keyword_gen(const char* str, struct kwdata *keywords, long len){
 				token[i] = tolower((unsigned char)token[i]);
 			}
 			if (!keyword_exists(keywords, token))
-				add_keyword(keywords, token);
+				keyword_add(keywords, token);
 		}
 		token = strtok(NULL, delim);
-	}
-}
-
-void keyword_css(const char* id, struct kwdata *keywords, long len) {
-	const char *css1 = "input[value='";
-	const char *css2 = "' i] ~ #booksearchresults #";
-	const char *css3 = "{ display: list-item }";
-	for (int i = 0; i < keywords->nkeyword; ++i) {
-		char *kw = keywords->vkeyword[i];
-		int kw_len = strlen(kw);
-		for (int j = kw_len; j >= len; j--) {
-			if (j == len && i == keywords->nkeyword-1)
-				printf("%s%s%s%s\n%s\n",css1,kw,css2,id,css3);
-			else
-				printf("%s%s%s%s%s\n",css1,kw,css2,id,",");
-			kw[j-1] = '\0';
-		}
 	}
 }
 
@@ -111,18 +88,18 @@ int main(int argc, char *argv[])
 	long min_keyword_len = 3;
 
 	opterr = 0;
-	for (int opt; (opt = getopt(argc, argv, "rl:")) != -1; ) {
+	for (int opt; (opt = getopt(argc, argv, "l:ra")) != -1; ) {
 		char *endptr;
 		switch (opt) {
-			case 'r':
-				raw = true;
-				break;
 			case 'l':
 				min_keyword_len = strtol(optarg, &endptr, 10);
 				if (endptr[0] != '\0') {
 					fprintf(stderr, "ngram: invalid flag value for -l\n\n%s", usage);
 					return 1;
 				}
+				break;
+			case 'r':
+				raw = true;
 				break;
 			case '?':
 				fprintf(stderr, "ngram: invalid flag -%c\n\n%s", optopt, usage);
@@ -151,13 +128,11 @@ int main(int argc, char *argv[])
 	json_object *subtitle;
 	json_object *author;
 	json_object *booktags;
-	json_object *lcc;
-
 
 	struct kwdata keywords = {{{0}},0};
 
 	for (int k=0; k<nbooks; k++) {
-		keyword_init(&keywords);
+		keywords_init(&keywords);
 		jbook = json_object_array_get_idx(jbooks, k);
 
 		title = json_object_object_get(jbook, "title");
@@ -180,18 +155,21 @@ int main(int argc, char *argv[])
 			keyword_gen(tag_str,&keywords,min_keyword_len);
 		}
 
-		if (!raw) {
-			lcc = json_object_object_get(jbook, "lccnumber");
-			const char *lcc_str = json_object_get_string(lcc);
-			keyword_css(lcc_str,&keywords,min_keyword_len);
-		} else {
-			for (int i = 0; i < keywords.nkeyword; ++i) {
-				char *kw = keywords.vkeyword[i];
-				int kw_len = strlen(kw);
-				for (int j = kw_len; j >= min_keyword_len; j--) {
-					printf("%s\n",kw);
-					kw[j-1] = '\0';
-				}
+		const char *cssid = raw ? "" : json_object_get_string(json_object_object_get(jbook, "lccnumber"));
+		const char *css1 = raw ? "" : "input[value='";
+		const char *css2 = raw ? "" : "' i] ~ #booksearchresults #";
+		const char *css3 = raw ? "" : ",";
+		const char *css4 = raw ? "" : "{ display: list-item }";
+
+		for (int i = 0; i < keywords.nkeyword; ++i) {
+			char *kw = keywords.vkeyword[i];
+			int kw_len = strlen(kw);
+			for (int j = kw_len; j >= min_keyword_len; j--) {
+				if (j == min_keyword_len && i == keywords.nkeyword-1)
+					printf("%s%s%s%s%s\n",css1,kw,css2,cssid,css4);
+				else
+					printf("%s%s%s%s%s\n",css1,kw,css2,cssid,css3);
+				kw[j-1] = '\0';
 			}
 		}
 
